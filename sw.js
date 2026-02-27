4dc0c0629d3638abf2317b360ebfec63b34adb54
nlt42886 <nlt42886@gmail.com>
2026-02-26 23:39:44 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'mustang-pm-v2';
+const ASSETS = [
+  '/PM-Tracker/',
+  '/PM-Tracker/index.html',
+  '/PM-Tracker/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+  );
+});
